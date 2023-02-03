790e60073ffc5185b16a69c3a37f5468cd0f57de
christos <cmagganas@gmail.com>
2023-02-03 12:25:45 -0800

    adding week 7 assignment

--- assignments/week-07-intro-dl/nb/fuel-efficiency-prediction-streamlit.py
+++ assignments/week-07-intro-dl/nb/fuel-efficiency-prediction-streamlit.py
@@ -12,8 +12,8 @@ st.image(img_fuel, width=700)
 st.markdown("<h1 style='text-align: center; color: black;'>Fuel Efficiency</h1>", unsafe_allow_html=True)
 
 # Import train dataset to DataFrame
-train_df = pd.read_csv("../dat/train.csv.gz", compression="gzip")
-model_results_df = pd.read_csv("../dat/model_results.csv")
+train_df = pd.read_csv("../nb/dat/train_df.csv.gz", compression="gzip")
+model_results_df = pd.read_csv("../nb/dat/model_results.csv")
 
 # Create sidebar for user selection
 with st.sidebar:
@@ -23,7 +23,7 @@ with st.sidebar:
     # Available models for selection
 
     # YOUR CODE GOES HERE!
-    models = ["DNN", "TPOT"]
+    models = ["DNN", "TPOT: ExtraTreesRegressor"]
 
     # Add model select boxes
     model1_select = st.selectbox(
@@ -33,7 +33,7 @@ with st.sidebar:
     
     # Remove selected model 1 from model list
     # App refreshes with every selection change.
-    models.remove(model1_select)
+    # models.remove(model1_select)
     
     model2_select = st.selectbox(
         "Choose Model 2:",
@@ -60,6 +60,13 @@ with tab1:
 with tab2:    
     
     # YOUR CODE GOES HERE!
+    cols = ['mse','mae','r2']
+
+    # Data Section Header
+    st.header("Model Results")
+
+    # Display your performance metrics
+    st.dataframe(model_results_df)
 
     # Columns for side-by-side model comparison
     col1, col2 = st.columns(2)
@@ -69,6 +76,33 @@ with tab2:
         st.header(model1_select)
 
         # YOUR CODE GOES HERE!
+        model1_results = model_results_df[model_results_df["model"] == model1_select][cols]
+
+        fig1 = px.imshow(model1_results, text_auto=True)
+        
+        # add custom x-axis title
+        fig1.add_annotation(dict(font=dict(color="black",size=14),
+                                x=0.5,
+                                y=-0.00005,
+                                showarrow=False,
+                                text="Predicted value",
+                                xref="paper",
+                                yref="paper"))
+
+        # add custom y-axis title
+        fig1.add_annotation(dict(font=dict(color="black",size=14),
+                                x=-0.25,
+                                y=0.5,
+                                showarrow=False,
+                                text="True value",
+                                textangle=-90,
+                                xref="paper",
+                                yref="paper"))
+
+
+        # Write plotly chart and fit to the container width.
+        st.plotly_chart(fig1, use_container_width=True)
+
 
 
     # Build confusion matrix for second model
@@ -76,6 +110,33 @@ with tab2:
         st.header(model2_select)
 
         # YOUR CODE GOES HERE!
+        model2_results = model_results_df[model_results_df["model"] == model2_select][cols]
+
+        fig2 = px.imshow(model2_results, text_auto=True)
+
+        # add custom x-axis title
+        fig2.add_annotation(dict(font=dict(color="black",size=14),
+                                x=0.5,
+                                y=-0.00005,
+                                showarrow=False,
+                                text="Predicted value",
+                                xref="paper",
+                                yref="paper"))
+
+        # add custom y-axis title
+        fig2.add_annotation(dict(font=dict(color="black",size=14),
+                                x=-0.25,
+                                y=0.5,
+                                showarrow=False,
+                                text="True value",
+                                textangle=-90,
+                                xref="paper",
+                                yref="paper"))
+
+
+        # Write plotly chart and fit to the container width.
+        st.plotly_chart(fig2, use_container_width=True)
+
 
 
 with tab3: 
@@ -84,7 +145,9 @@ with tab3:
         # Include plots for local and global explanability!
      
     st.header(model1_select)
+    img_dnn_shap = Image.open('./dat/dnn_shap.png')
+    st.image(img_dnn_shap, width=1000)
     
     st.header(model2_select)
-
-    
+    img_tpot_shap = Image.open('./dat/dnn_shap.png')
+    st.image(img_tpot_shap, width=1000)
